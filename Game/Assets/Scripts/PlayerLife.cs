936025557f6881140229dd17b00ef51416ced2a7
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2024-01-19 10:43:34 +0000

    making a spotlight
    making a spotlight

---
+++ Game/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<PlayerMovement>().enabled = false;
+    }
+}
