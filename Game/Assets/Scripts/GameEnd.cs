3293ee514fe567e80d1fe9ac920d1b6f0b890446
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2024-01-23 10:45:51 +0000

    GameEnd
    ez

---
+++ Game/Assets/Scripts/GameEnd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            End();
+        }
+    }
+
+    void End()
+    {
+        SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
+    }
+}
